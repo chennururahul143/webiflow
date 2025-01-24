e741d3ef27cd2d713e0f28b4e2fe74da20aef927
Chennuru Rahul <62155848+chennururahul143@users.noreply.github.com>
2025-01-24 22:37:38 +0530

    Update image.jsx

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Image = ({ title, largeImage, smallImage }) => {
+export const Image = ({ title, smallImage, hyperlink}) => {
   return (
     <div className="portfolio-item">
       <div className="hover-bg">
         {" "}
-        <a href="#" title={title} data-lightbox-gallery="gallery1">
+        <a href= {hyperlink} title={title} data-lightbox-gallery="gallery1">
           <div className="hover-text">
             <h4>{title}</h4>
           </div>
